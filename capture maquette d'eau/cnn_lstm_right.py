c9c62a4c9667a55e1b2ee5f7fd6a176b3950ff95
Jeanne-Louise Dahan <jeanne-louise.dahan@telecom-sudparis.eu>
2020-01-20 18:04:26 +0100

    Avancement cnn et lstm

--- capture maquette d'eau/cnn_lstm_right.py
+++ capture maquette d'eau/cnn_lstm_right.py
@@ -1,20 +1,18 @@
 from __future__ import print_function
-import numpy as np
+import time
+
+start = time.time()
+from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
 
-np.random.seed(1337)  # for reproducibility
-
-
 from keras.models import Sequential
-from keras.layers import Convolution1D,MaxPooling1D
+from keras.layers import MaxPooling1D, Conv1D, TimeDistributed, Activation, Flatten
 import pandas as pd
 from sklearn.preprocessing import Normalizer
 from keras.layers import Dense,Dropout
-from keras.utils import np_utils
 import numpy as np
 from keras.layers import LSTM
-from sklearn.metrics import (precision_score, recall_score,f1_score, accuracy_score,mean_squared_error,mean_absolute_error)
-from sklearn import metrics
+
 
 data = pd.read_table('Capture Data/right_lstm_raspi_data_grosse.txt', delimiter=",", header=None)
 
@@ -36,7 +34,7 @@ X = data.iloc[:, 0:-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=0)
 
 scaler = Normalizer().fit(X_train)
 X_train = scaler.transform(X_train)
@@ -56,16 +54,37 @@ lstm_output_size = 70
 #Model definition
 
 cnn_lstm = Sequential()
-cnn_lstm.add(Convolution1D(64, 3, border_mode="same",activation="relu",input_shape=(13, 1)))
+cnn_lstm.add(Conv1D(64, 3, activation="relu", input_shape=(13, 1), padding="same"))
 cnn_lstm.add(MaxPooling1D(pool_length=2))
-cnn_lstm.add(LSTM(lstm_output_size))
-cnn_lstm.add(Dropout(0.1))
-cnn_lstm.add(Dense(1, activation="sigmoid"))
+cnn_lstm.add(LSTM(300, return_sequences=True))
+cnn_lstm.add(Dropout(0.1)) #Avoid overfitting in the model
+cnn_lstm.add(LSTM(300, return_sequences=True))
+cnn_lstm.add(Flatten())
+cnn_lstm.add(TimeDistributed(Dense(100)))
+cnn_lstm.add(Activation('linear'))
+cnn_lstm.add(Dense(1, activation="linear"))
 
-# define optimizer and objective, compile cnn
+print(cnn_lstm.summary())
 
 cnn_lstm.compile(loss="mean_absolute_error", optimizer="adam", metrics=['mae'])
 
 # train
-cnn_lstm.fit(X_train, y_train, epochs=100, validation_data=(X_val, y_val))
+history = cnn_lstm.fit(X_train, y_train, epochs=100, batch_size=8, verbose = 0)
+
+scores = cnn_lstm.evaluate(X_val, y_val, verbose=1)
+print("%s: %.2f" % (cnn_lstm.metrics_names[1], scores[1]))
+
+running_time = time.time() - start
+print("Total running time : ", running_time)
+
+plot = True
+if plot:
+    pyplot.plot(history.history['mae'], "-r", label="Mean Absolute Error")
+    pyplot.legend(loc="upper right")
+
+    pyplot.xlabel("Epochs")
+    pyplot.title("Evolution of the Mean Absolute Error (LSTM)")
+    pyplot.show()
+
+
 cnn_lstm.save("cnn_lstm_model_right.hdf5")
